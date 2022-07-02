34d3e8173c6edff5ab38800d43526ae1fe703514
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-07-02 08:54:42 +0500

    Slicing_list04

--- Slicing_list04.py
+++ Slicing_list04.py
@@ -1,9 +1,11 @@
 def main(list1):
     """
-    A list of several elements is given. Return the three elements from the beginning.
+    A list of several elements is given.
+     Return the three elements from the beginning.
     Args:
         list1(list): parameter
     Returns:
         list: return answer.
     """
-    return
+    return list1[:3]
+print(main([1,12,3,9,5,9]))
